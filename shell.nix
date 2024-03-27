6e164f90f49ec8ddd43e6df7f838c869c46dd04b
JackDrury <31498799+JackDrury@users.noreply.github.com>
2024-03-27 14:31:15 +1100

    nix shell modification

--- shell.nix
+++ shell.nix
@@ -4,26 +4,16 @@
 pkgs.mkShellNoCC {
   packages = with pkgs; [
     (python3.withPackages (ps: [
-	ps.openai
-	ps.psycopg2
-	]))
-    postgresql
+	    ps.openai
+	    ps.sqlite3
+	  ]))
     sqlite
   ];
 
   PGHOST = "/home/jdd/jems";
 
   shellHook = ''
-    # Create a database with the data stored in the current directory
-#    initdb -D .tmp/cooldb
-
-    # Start PostgreSQL running as the current user
-    # and with the Unix socket in the current directory
-#    pg_ctl -D .tmp/cooldb -l logfile -o "--unix_socket_directories='$PWD'" start
-
-    # create a database
-#    createdb cooldb
-    echo "hello, nix-shell importing all packages from 'nix-unstable' with python, sqlite3, psycopg2, openai, postgresql and sqlite has begun"
+    echo "hello, nix-shell importing all packages from 'nix-unstable' with python, sqlite3, openai and sqlite has begun"
     echo "run the following commands to do the stuff you usually want to do:"
     echo "initdb -D .tmp/cooldb"
     echo "pg_ctl -D .tmp/cooldb -l logfile -o \"--unix_socket_directories='$PWD'\" start"
